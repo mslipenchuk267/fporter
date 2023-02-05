34da32b5fb4f4c09324c80960a6c16e4e5cc00d6
mslipenchuk267 <mslipenchuk267@gmail.com>
2023-02-05 18:43:25 -0500

    main.go takes arguments and created data folder with test data

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, I'm fporter!")
+	// Parse Arguments
+	args1 := os.Args
+	args2 := os.Args[1:]
+
+	fmt.Println("Arguments with Exe Call:")
+	fmt.Println(args1)
+	fmt.Println("Arguments without Exe Call:")
+	fmt.Println(args2)
 }
