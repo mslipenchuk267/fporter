6adeaf813c09b5125b773f9b75ebaeba5d71ead4
mslipenchuk267 <mslipenchuk267@gmail.com>
2023-02-05 22:57:49 -0500

    Added cp, mv, rp subcommand CLI arg & flag parsing

--- main.go
+++ main.go
@@ -1,17 +1,47 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 )
 
 func main() {
-	// Parse Arguments
-	args1 := os.Args
-	args2 := os.Args[1:]
+	// Define Subcommands & flags
+	cpCmd := flag.NewFlagSet("cp", flag.ExitOnError)
+	cpIn := cpCmd.String("in", "", "Input Directory")
+	cpOut := cpCmd.String("out", "", "Output Directory")
 
-	fmt.Println("Arguments with Exe Call:")
-	fmt.Println(args1)
-	fmt.Println("Arguments without Exe Call:")
-	fmt.Println(args2)
+	mvCmd := flag.NewFlagSet("mv", flag.ExitOnError)
+	mvIn := mvCmd.String("in", "", "Input Directory")
+	mvOut := mvCmd.String("out", "", "Output Directory")
+
+	rpCmd := flag.NewFlagSet("rp", flag.ExitOnError)
+	rpIn := rpCmd.String("in", "", "Input Directory")
+	rpRgx := rpCmd.String("rgx", "", "Regex Schema")
+
+	switch os.Args[1] {
+	case "cp":
+		fmt.Println("Invoking Copy Procedure")
+		cpCmd.Parse(os.Args[2:])
+		fmt.Println("\tInput Dir:", *cpIn)
+		fmt.Println("\tOutput Dir:", *cpOut)
+		fmt.Println("\tTrailing Args:", cpCmd.Args())
+	case "mv":
+		fmt.Println("Invoking Move Procedure")
+		mvCmd.Parse(os.Args[2:])
+		fmt.Println("\tInput Dir:", *mvIn)
+		fmt.Println("\tOutput Dir:", *mvOut)
+		fmt.Println("\tTrailing Args:", mvCmd.Args())
+	case "rp":
+		fmt.Println("Invoking Replace Procedure")
+		rpCmd.Parse(os.Args[2:])
+		fmt.Println("\tInput Dir:", *rpIn)
+		fmt.Println("\tRegex:", *rpRgx)
+		fmt.Println("\tTrailing Args:", rpCmd.Args())
+	default:
+		fmt.Println("Arg 1 Error: Please specify valid procedure: Copy (cp), Move (mv), Replace (rp)")
+		os.Exit(1)
+	}
+
 }
